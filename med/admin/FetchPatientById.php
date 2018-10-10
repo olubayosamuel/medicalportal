16b41576e21ef9e65d356abd7937a21d0b4678d6
Oluwapelumi Olaoye <pelumiolaoye@payloader.ng>
2018-10-10 12:42:37 +0100

    Bug fixes

--- med/admin/FetchPatientById.php
+++ med/admin/FetchPatientById.php
@@ -12,16 +12,16 @@ require ('redirect.php');
 $table_name = "patients";
 
 if($_POST['patient']) {
-	var_dump('Check patient id '. patient_id);
-	exit;
 	$patient_id = stripslashes($patient_id);
     $patient_id = $_POST['patient'];
 
 	//Store patient id in session
 	$_SESSION['patient-id'] = $patient_id;
+	//Redirect to manage patient page
 	redirect('manage-patients.php');
 	exit;
 } else {
+    //Redirect to patient id page
     redirect('fetch-patient-id.php');
     exit;
 }
